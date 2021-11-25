41755b4ec30d4ba62c35ed91531189f22af0780f
Zoe Schmitt <zoeschmitt@hotmail.com>
2021-11-24 23:45:01 -0600

    added support for mainnet-fork

--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -1,13 +1,17 @@
 from brownie import accounts, config, network, MockV3Aggregator
 from web3 import Web3
 
+FORKED_LOCAL_ENVIRONMENTS = ["mainnet-fork"]
 LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development", "ganache-local"]
 DECIMALS = 8
 STARTING_PRICE = 200000000
 
 
 def get_account():
-    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+    if (
+        network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS
+        or network.show_active() in FORKED_LOCAL_ENVIRONMENTS
+    ):
         return accounts[0]
     else:
         return accounts.add(config["wallets"]["from_key"])
@@ -18,7 +22,5 @@ def deploy_mocks():
     print("Deploying mocks...")
     # MockV3Aggregator is just a list of all the MockV3Aggregator's we've deployed
     if len(MockV3Aggregator) <= 0:
-        MockV3Aggregator.deploy(
-            DECIMALS, STARTING_PRICE, {"from": get_account()}
-        )
+        MockV3Aggregator.deploy(DECIMALS, STARTING_PRICE, {"from": get_account()})
     print("Mocks deployed")
