ff361c2189c31e6dede0958c20af199c990bea08
Zoe Schmitt <zoeschmitt@hotmail.com>
2021-11-24 23:07:39 -0600

    initial commit

---
+++ scripts/helpful_scripts.py
@@ -0,0 +1,24 @@
+from brownie import accounts, config, network, MockV3Aggregator
+from web3 import Web3
+
+LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development", "ganache-local"]
+DECIMALS = 8
+STARTING_PRICE = 200000000
+
+
+def get_account():
+    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        return accounts[0]
+    else:
+        return accounts.add(config["wallets"]["from_key"])
+
+
+def deploy_mocks():
+    print(f"The active network is {network.show_active()}")
+    print("Deploying mocks...")
+    # MockV3Aggregator is just a list of all the MockV3Aggregator's we've deployed
+    if len(MockV3Aggregator) <= 0:
+        MockV3Aggregator.deploy(
+            DECIMALS, STARTING_PRICE, {"from": get_account()}
+        )
+    print("Mocks deployed")
